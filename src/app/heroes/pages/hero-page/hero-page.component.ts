534bf1dea72c97322f855ba33b9ba6700dc31bee
cinta <cintatafur@gmail.com>
2023-11-29 16:09:34 +0100

    ➕ pipe(obtener url)

--- src/app/heroes/pages/hero-page/hero-page.component.ts
+++ src/app/heroes/pages/hero-page/hero-page.component.ts
@@ -1,16 +1,53 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs';
+import { Hero } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'heroes-hero-page',
   templateUrl: './hero-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class HeroPageComponent implements OnInit {
 
-  constructor() { }
+  public hero? : Hero
+
+  constructor(
+    private heroesService: HeroesService,
+    private acRoute: ActivatedRoute,
+    private router: Router
+  ) {}
+
+/*   ngOnInit(): void {
+    this.acRoute.params
+      .pipe(switchMap(({ id }) => this.heroesService.getHeroById(id)))
+      .subscribe((hero) => {
+        if(!hero ) return this.router.navigate(['/heroes.list'])
+
+        this.hero = hero;
+        console.log({hero});
+        return;
+      });
+  } */
 
   ngOnInit(): void {
+    this.acRoute.params
+      .pipe(
+        switchMap( ({ id }) => this.heroesService.getHeroById( id )),
+      )
+      .subscribe( hero => {
+
+        if ( !hero ) return this.router.navigate([ '/heroes/list' ]);
+
+        this.hero = hero;
+        console.log({hero});
+        return;
+      })
+  }
+
+  goBack():void {
+    this.router.navigateByUrl('heroes/list')
   }
 
 }
--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,21 +1,23 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
 import { Hero } from '../interfaces/hero.interface';
 import { environment } from 'src/environments/environment';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class HeroesService {
-
   private url: string = environment.URL;
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-
-  getHeroe(): Observable<Hero[]>{
-    /*   return this.httpClient.get<Hero[]>(`${this.url}/heroes`) */
-      return this.http.get<Hero[]>(`${this.url}/heroes`);
+  getHeroe(): Observable<Hero[]> {
+    return this.http.get<Hero[]>(`${this.url}/heroes`);
   }
 
-
+  getHeroById(id: string): Observable<Hero | undefined> {
+    return this.http.get<Hero>(`${this.url}/heroes/${id}`)
+    .pipe(
+      catchError( error=>of(undefined) )
+    )
+  }
 
 }
