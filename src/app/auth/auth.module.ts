7f7fb7b22effe911757b3630f18333f7cb58bc47
cinta <cintatafur@gmail.com>
2023-11-29 10:29:36 +0100

    ✏️ Create(login & register):con routerLink

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,16 +1,17 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth.routing.module';
+import { CommonModule } from '@angular/common';
 import { LayoutPagesComponent } from './pages/layout-pages/layout-pages.component';
-
-
-
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { MaterialModule } from '../material/material.module';
+import { NgModule } from '@angular/core';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
 
 @NgModule({
-  declarations: [LayoutPagesComponent],
-  imports: [
-    CommonModule,
-    AuthRoutingModule
-  ]
+  declarations: [
+    LayoutPagesComponent,
+    LoginPageComponent,
+    RegisterPageComponent,
+  ],
+  imports: [CommonModule, AuthRoutingModule, MaterialModule],
 })
-export class AuthModule { }
+export class AuthModule {}
