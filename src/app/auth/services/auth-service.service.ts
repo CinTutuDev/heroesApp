625f24864698c8dffa932d968495186e97565fa1
cinta <cintatafur@gmail.com>
2023-12-08 14:23:46 +0100

    🩺 Create(service): en Auth service para autentificacion

---
+++ src/app/auth/services/auth-service.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Observable, tap } from 'rxjs';
+
+import { environment } from 'src/environments/environment';
+import { User } from '../interfaces/user';
+
+@Injectable({ providedIn: 'root' })
+export class AuthService {
+  private url = environment.URL;
+  private user?: User;
+
+  constructor(private http: HttpClient) {}
+
+  get currentUser(): User | undefined {
+    if (!this.user) return undefined;
+    return structuredClone(this.user);
+  }
+
+  login(email: string, passw: string): Observable<User> {
+    return this.http.get<User>(`${this.url}/users/1`).pipe(
+      tap((user) => (this.user = user)),
+      tap((user) => localStorage.setItem('token', user.id.toString()))
+    );
+  }
+}
